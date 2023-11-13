cb901f10724d036bc747062a358c8d49eb4b8023
MathisSparky <145775791+MathisSparky@users.noreply.github.com>
2023-11-12 20:25:33 -0500

    Add files via upload

---
+++ src/build/build.bat
@@ -0,0 +1,4 @@
+@echo off
+gcc -c ./src/compiler/main/index.c -o ./src/compiler/bin/index.o
+gcc -c ./src/compiler/lexer/lexer.c -o ./src/compiler/bin/lexer.o
+gcc ./src/compiler/bin/lexer.o ./src/compiler/bin/index.o -o ./src/compiler/build/run.exe
